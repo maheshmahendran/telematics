5d2b4fda6acff4ca6608ae49872a7009ab1f301d
Mahesh Mahendran <mmahesh@gocontec.com>
2018-01-10 17:38:51 +0530

    Database created along with entity definition but thought of using annotation so let do the config first

---
+++ Backend/src/AppBundle/Entity/Devices.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Devices
+{
+    private $id;
+    private $deviceId;
+    private $deviceLabel;
+    private $status;
+}
