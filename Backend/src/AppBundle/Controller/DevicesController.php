53940b0d1b1fcb1c1587c1be8982348e6ff145c5
Mahesh Mahendran <mmahesh@gocontec.com>
2018-01-11 11:30:02 +0530

    Pagefunda and Controller error corrected

--- Backend/src/AppBundle/Controller/DevicesController.php
+++ Backend/src/AppBundle/Controller/DevicesController.php
@@ -3,6 +3,15 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Device;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Nelmio\ApiDocBundle\Annotation as Doc;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * @Route("/devices")
